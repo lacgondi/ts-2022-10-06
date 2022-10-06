b9a4ac87e7e3636d1a111c2c2a8a6b51ad8f504f
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-06 12:47:08 +0200

    Edit index.ts
    Write tests for Book class.
    It outputs all the books, the books with a rating of 8 or higher and the best book to the console

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,32 @@ import { Book } from './Book';
 let bookList:Book[] = [new Book("Dune", 10), new Book("Solaris",0), new Book("Where the Crawdads sing",12)];
 
 for (let index = 0; index < 30; index++) {
-    bookList.push(new Book("Book #"+index, (Math.random()*10)+1));
+    bookList.push(new Book("Book #"+index, Math.floor((Math.random()*10))+1));
 }
 
+bookList.forEach(element => {
+    console.log(element.toString());
+});
 
+//8 rating or higher
+bookList.forEach(element => {
+    if(element.getRating()>7){
+        console.log(element.toString());
+    }
+});
 
+//Best book
+
+function bestOf(list:Book[]):string{
+    let max=0;
+    let maxIndex=0;
+    for (let index = 0; index < list.length; index++) {
+        if(list[index].getRating()>max){
+            max = list[index].getRating();
+            maxIndex=index;
+        }
+    }
+    return list[maxIndex].toString();
+}
+
+console.log("A legjobb könyv:", bestOf(bookList));
