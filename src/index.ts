8eb71824ddd971f1afa175fe5d881a4d78f3e21c
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-06 12:38:53 +0200

    Edit index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-import './Book.ts';
+import { Book } from './Book';
+
+let bookList:Book[] = [new Book("Dune", 10), new Book("Solaris",0), new Book("Where the Crawdads sing",12)];
+
+for (let index = 0; index < 30; index++) {
+    bookList.push(new Book("Book #"+index, (Math.random()*10)+1));
+}
+
+
 
