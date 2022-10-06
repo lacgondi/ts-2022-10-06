59b0bb3bcd5dc38540f5b450bdc13e2e00ed0ca8
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-06 12:27:00 +0200

    Add constructor and getter setters in index.ts

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+class Book {
+    #name:string;
+    #rating:number;
+    constructor(name:string, rating:number) {
+        this.#name=name;
+        if (rating>10) {
+            this.#rating = 10;
+        }else if(rating<1){
+            this.#rating=1;
+        }else{
+            this.#rating=rating;
+        }
+    }
+
+    public getName() : string {
+        return this.#name;
+    }
+    
+    public getRating() : number {
+        return this.#rating;
+    }
+
+    
+    public setName(v : string) {
+        this.#name = v;
+    }
+
+    public setRating(v : number) {
+        if (v>10) {
+            this.#rating = 10;
+        }else if(v<1){
+            this.#rating=1;
+        }else{
+            this.#rating=v;
+        }
+    }
+    
+}
